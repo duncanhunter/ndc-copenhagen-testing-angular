72be7242d0b7aee68a92ff446aa6565b7892ebc6
Duncan Hunter <duncan@duncanhunter.com.au>
2017-02-24 12:34:21 +0100

    add e2e

---
+++ e2e/joke.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { browser, by, element } from 'protractor';
+
+describe(`Page: Joke Page`, () => {
+  it(`should have a title of 'Chuck Norris Jokes'`, () => {
+      browser.get('/');
+
+      const title = element(by.css('h1')).getText();
+
+      expect(title).toEqual('Chuck Norris Jokes');
+  });
+
+  it(`should have a different second joke`, async () => {
+    browser.get('/');
+
+    const joke1 = element(by.css('p')).getText();
+
+    element(by.css('button')).click();
+
+    const joke2 = await element(by.css('p')).getText();
+
+    expect(joke1).not.toEqual(joke2);
+  });
+
+});
